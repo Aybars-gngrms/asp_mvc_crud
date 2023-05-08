8d01889cccb42c465bcfebb2356f88dd88c978be
Aybars-gngrms <ayhars.gunormus04@gmail.com>
2023-05-08 17:39:21 +0300

    mvc_crud_project

---
+++ crud/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using crud.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options) 
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+    
